b2002b5fc932058c4d870a88c8a30e58e3319abe
rozaqi02 <rojaki1419@gmail.com>
2024-04-14 22:39:08 +0700

    Pengumpulan UTS Form

--- UTS/proses_login.php
+++ UTS/proses_login.php
@@ -4,7 +4,7 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['username'] === '2241760123' && $_POST['password'] === '2241760123') {
     $_SESSION['logged_in'] = true;
 
-    header('Location: index.html');
+    header('Location: form_pengunduran.html');
     exit();
 }
 
